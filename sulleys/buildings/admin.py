06ab48c93f19a93ccf9616a2d50c6860104c582e
Christy O'Reilly <christy@oreills.co.uk>
2019-04-21 17:43:46 +0100

    Add better CMS integration for models

--- sulleys/buildings/admin.py
+++ sulleys/buildings/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
+
+from cms.admin.placeholderadmin import FrontendEditableAdminMixin
+
 from buildings.models import Building
 
-class BuildingAdmin(admin.ModelAdmin):
+class BuildingAdmin(FrontendEditableAdminMixin, admin.ModelAdmin):
     prepopulated_fields = {'slug': ('name',)}
 
 admin.site.register(Building, BuildingAdmin)
