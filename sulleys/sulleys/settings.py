92135b5b8608a35a6e57955f21e4205d7a653151
Christy O'Reilly <christy@oreills.co.uk>
2019-04-21 09:08:12 +0100

    Fix for relative template directory

--- sulleys/sulleys/settings.py
+++ sulleys/sulleys/settings.py
@@ -83,7 +83,7 @@ ROOT_URLCONF = 'sulleys.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
