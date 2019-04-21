98496c0202aeb8267808d62179802a2c583c406a
Christy O'Reilly <christy@oreills.co.uk>
2019-04-21 18:02:03 +0100

    Add filterable tags

--- sulleys/buildings/urls.py
+++ sulleys/buildings/urls.py
@@ -4,5 +4,6 @@ from buildings import views
 
 urlpatterns = [
     path('', views.BuildingList.as_view(), name='index'),
+    path('tags/<str:tag>', views.BuildingListByTag.as_view(), name='list_by_tag'),
     path('<str:slug>/', views.BuildingDetail.as_view(), name='detail'),
 ]
--- sulleys/buildings/views.py
+++ sulleys/buildings/views.py
@@ -1,11 +1,22 @@
-from django.shortcuts import render
-
+from django.shortcuts import get_object_or_404
 from django.views.generic import DetailView, ListView
+
+from taggit.models import Tag
+
 from buildings.models import Building
+
 
 
 class BuildingList(ListView):
     model = Building
+
+
+class BuildingListByTag(ListView):
+    model = Building
+
+    def get_queryset(self):
+        self.tag = get_object_or_404(Tag, slug=self.kwargs['tag'])
+        return Building.objects.filter(tags=self.tag)
 
 
 class BuildingDetail(DetailView):
